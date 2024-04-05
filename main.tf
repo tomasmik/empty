46f807bf4fdf1e60cb69ca8b65d99ff45aaf43c5
Tomas <40318863+tomasmik@users.noreply.github.com>
2024-04-05 15:28:38 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,12 @@ provider "aws" {
     web_identity_token_file = "/mnt/workspace/spacelift.oidc"
   }
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
