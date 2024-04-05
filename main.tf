6ec88cbdbae7801606b4cbd5545eb855b4cee2af
Tomas <40318863+tomasmik@users.noreply.github.com>
2024-04-05 15:36:39 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-provider "aws" {
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 resource "aws_s3_bucket" "example" {
