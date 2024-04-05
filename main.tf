8996065b11687686cb1ff274946c828e80d8327c
Tomas <40318863+tomasmik@users.noreply.github.com>
2024-04-05 15:33:17 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 provider "aws" {
   assume_role_with_web_identity {
-    role_arn = var.aws_role_arn
     web_identity_token_file = "/mnt/workspace/spacelift.oidc"
   }
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-buckeasdfasdfasdft"
+  bucket = var.bucket_name
 
   tags = {
     Name        = "My bucket"
