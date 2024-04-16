8ee554045c6792199bdb45ba326a7fd6fc74c8cc
Tomas <40318863+tomasmik@users.noreply.github.com>
2024-04-16 12:12:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,23 +7,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-
-resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-variable "bucket_name" {
-  type = string
-}
 
 resource "random_string" "random" {
   length           = 16
