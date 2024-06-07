ae7c1398508707a57a6d5c1d9cbde5dca0a6f12d
Tomas <40318863+tomasmik@users.noreply.github.com>
2024-06-07 10:45:11 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ variable "bucket_name" {
 resource "random_string" "random" {
   length           = 16
   special          = true
-  override_special = "/@£$"
+  override_special = "/@£"
 }
 
 resource "random_string" "random2" {
