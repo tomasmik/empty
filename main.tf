3a9ad12425353b0a16caeaa77ff2968df809ae24
Tomas <40318863+tomasmik@users.noreply.github.com>
2024-11-12 13:47:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,16 @@ resource "random_string" "random" {
   special          = true
   override_special = "/@£$"
 }
+
+resource "random_string" "random2" {
+  length           = 11
+  special          = true
+  override_special = "/@£$"
+}
+
+
+resource "random_string" "random3" {
+  length           = 13
+  special          = true
+  override_special = "/@£$"
+}
