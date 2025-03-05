9b72eabac28520e4aed3714c658ced1ba598d433
Tomas <40318863+tomasmik@users.noreply.github.com>
2025-03-05 17:48:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "spacelift_space" "dev-test99999" {
 }
 
 resource "spacelift_space" "dev-test888" {
-  name = "terraform-dev-teaaaahgta"
+  name = "terraform-dev-teaaaaaahgta"
   parent_space_id = "root"
 }
 
