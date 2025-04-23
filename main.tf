86691d2b810acfcf0de1bf842bbbe23ab1ef2eab
Tomas <40318863+tomasmik@users.noreply.github.com>
2025-04-23 11:47:30 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,29 +6,12 @@ terraform {
   }
 }
 
-resource "spacelift_space" "dev-test99999" {
-  name = "terraform-dev-teaaata"
-  parent_space_id = "root"
-}
-
-resource "spacelift_space" "dev-test888" {
-  name = "terraform-dev-teaaahgta"
-  parent_space_id = "root"
-}
 
 resource "spacelift_space" "dev-test" {
   name = "terraform-dev-teaaaaata"
   parent_space_id = "root"
 }
 
-output "spacelift_space_dev_test99999_id" {
-  value = spacelift_space.dev-test99999.id
-  description = "asdf"
-}
-
-output "spacelift_space_dev_test888_id" {
-  value = spacelift_space.dev-test888.id
-}
 
 output "spacelift_space_dev_test_id" {
   value = spacelift_space.dev-test.id
