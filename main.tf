45ed5bd340187ef0d98a3557192e042f999bd7b3
Tomas <40318863+tomasmik@users.noreply.github.com>
2025-04-24 10:02:29 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,20 @@ terraform {
 
 
 resource "spacelift_space" "dev-test" {
-  name = "terraform-dev-teaaaaata"
+  name = "terraform-dev-local1"
+  parent_space_id = "root"
+}
+
+resource "spacelift_space" "dev-testt" {
+  name = "terraform-dev-local2"
   parent_space_id = "root"
 }
 
 
 output "spacelift_space_dev_test_id" {
   value = spacelift_space.dev-test.id
+}
+
+output "spacelift_space_dev_testt_id" {
+  value = spacelift_space.dev-testt.id
 }
